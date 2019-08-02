24c242708b3d9933ebe33235df2c867f2fc03353
dmitriy365377 <tigr365377@gmail.com>
2019-08-02 11:53:47 +0300

    life cycle methods, componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,16 +4,13 @@ import axios from 'axios'
 import userPhoto from '../../assets/images/img_2.jpg'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props)
 
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
-
     }
-
 
 
     render() {
